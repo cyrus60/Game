89671d65d2f36c920ac80bef56f5dd08445c8212
cyrus60 <78514343+cyrus60@users.noreply.github.com>
2021-10-28 14:12:35 -0400

    Ball now bounces and gets heavier when space bar is pressed. Also, stars have been added and dissapear once collision with ball is detected.

--- game.py
+++ game.py
@@ -1,48 +1,104 @@
 import pygame, sys
 from pygame.locals import *
+import random
+
+black = 0, 0, 0
+purple = 200, 0, 250
+
+# Defines ball object          
+class Ball():
+    def __init__(self):
+        self.x = 250
+        self.y = 250
+        self.speed = 20
+        self.acceleration = 5
+        self.rect = ''
+
+    def draw(self):
+        self.ball = pygame.draw.circle(screen, (0, 0, 0), (self.x, self.y), 32)
+        self.rect = pygame.rect.Rect(self.ball)
+
+    def right(self):
+        self.x = self.x+25
+        if self.x > 500:
+            self.x = 0
+
+    def left(self):
+        self.x = self.x-25
+        if self.x < 0:
+            self.x = 500
+
+    def bounce(self, key):
+        if self.y > 459:
+            self.speed -= self.acceleration
+            self.speed = - self.speed
+        elif self.y <= 459:
+            self.acceleration = 5
+            self.speed += self.acceleration
+            if key[pygame.K_SPACE]:
+                self.acceleration = self.acceleration * 2
+                self.speed += self.acceleration
+        
+        self.y += self.speed
+            
+
+# Defines star object
+class Star(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.x = random.randrange(30, 470)
+        self.y = random.randrange(50, 435)
+        self.image = ''
+        self.rect = ''
+
+    def draw(self, rect):
+        image = pygame.image.load('img/star.png').convert_alpha()
+        self.image = pygame.transform.scale(image, (image.get_width() * .8, image.get_height() * .8))
+        self.rect = image.get_rect()
+        self.rect.center = (self.x, self.y)
+        screen.blit(self.image, self.rect)
+        if rect.colliderect(self.rect):
+            self.rect.center = (-100, -100)
+            screen.blit(self.image, self.rect)
+            self.x, self.y = random.randrange(1, 499), random.randrange(20, 435)
+            self.rect.center = (self.x, self.y)
+            screen.blit(self.image, self.rect)
 
 
-pygame.init()
+def main():
+    ball = Ball()
+    star = Star() 
 
-size = width, height = 500, 500
-speed = [2, 2]
-white = 255, 255, 255
+    # Main game loop
+    while True: 
+        pygame.time.delay(70)
 
-x = 50
-y = 50
+        # Events
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Waddup bitch")
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            ball.left()
 
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            ball.right()
 
-while True: 
-    pygame.time.delay(75)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
-    keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_LEFT]:
-        x = x-25
-        if x < 0:
-            x = 500
-    elif keys[pygame.K_RIGHT]:
-        x = x+25
-        if x > 500:
-            x = 0
-    elif keys[pygame.K_UP]:
-        y = y-25
-        if y < 0:
-            y = 500
-    elif keys[pygame.K_DOWN]:
-        y = y+25
-        if y > 500:
-            y = 0
+        # Game Logic
+        screen.fill(purple)
+        ball.draw()
+        ball.bounce(keys)
+        ballRect = ball.rect
+        star.draw(ballRect)
+        
+        pygame.draw.rect(screen, (0, 0, 0), (0, 460, 500, 100))
+        pygame.display.update()
 
 
-    screen.fill(white)
-    pygame.draw.circle(screen, (0, 0, 0), (x, y), 35)
-    pygame.draw.rect(screen, (0, 0, 0), (500, 20, 500, 20))
-    pygame.display.update()
+if __name__ == '__main__':
+    size = width, height = 500, 500
+    pygame.init
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("BallGame v 1.0")
+    main()
